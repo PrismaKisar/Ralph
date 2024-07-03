47a57a4e29f1995977a4231eb5939d1affe906dc
Alessandro Sarchi <alessandro.sarchi02@gmail.com>
2024-07-03 23:16:15 +0200

    sistemati i tick

--- Source/CustomLookAndFeel.cpp
+++ Source/CustomLookAndFeel.cpp
@@ -5,18 +5,39 @@ CustomLookAndFeel::CustomLookAndFeel() {
     knobBase = juce::ImageFileFormat::loadFrom(BinaryData::knobBase_png, BinaryData::knobBase_pngSize);
 }
 
-void CustomLookAndFeel::drawRotarySlider(Graphics& g, int x, int y, int width, int height, float sliderPosProportional, float rotaryStartAngle, float rotaryEndAngle, Slider &slider) {
+void CustomLookAndFeel::drawRotarySlider(Graphics& g, int x, int y, int width, int height,
+                                         float sliderPosProportional, float rotaryStartAngle,
+                                         float rotaryEndAngle, Slider &slider) {
     const float angle = rotaryStartAngle + sliderPosProportional * (rotaryEndAngle - rotaryStartAngle);
     
+    float radius = jmin(width, height) * 0.5f;
+    float centreX = x + width * 0.5f;
+    float centreY = y + height * 0.5f;
+    const float tickLength = 3.0f;
+    const float tickThickness = 2.0f;
+    const Colour tickColor = Colours::grey;
+
+    Path originalShape, tempShape;
+    originalShape.addRectangle(tickThickness * -0.5f, -radius, tickThickness, tickLength);
+
+    g.setColour(tickColor);
+    for (int i = 0; i < numTicks; ++i) {
+        tempShape = originalShape;
+        float tickAngle = rotaryStartAngle + (i / (float)(numTicks - 1)) * (rotaryEndAngle - rotaryStartAngle);
+        tempShape.applyTransform(AffineTransform::rotation(tickAngle).translated(centreX, centreY));
+        g.fillPath(tempShape);
+    }
+
     g.saveState();
     g.addTransform(AffineTransform::translation(x + width * 0.5f, y + height * 0.5f));
     g.addTransform(AffineTransform::rotation(angle));
 
-    float scaleFactor = jmin((float)width / knob.getWidth(), (float)height / knob.getHeight());
+    float scaleFactor = jmin((float)width / knob.getWidth(), (float)height / knob.getHeight()) * 0.8f;  // Scala l'immagine della knob
 
     g.addTransform(AffineTransform::scale(scaleFactor, scaleFactor));
     g.drawImageTransformed(knob, AffineTransform::translation(-knob.getWidth() * 0.5f, -knob.getHeight() * 0.5f));
     g.restoreState();
-    
-    g.drawImageWithin(knobBase, x, y, width, height, RectanglePlacement::stretchToFit);
+
+    g.drawImageWithin(knobBase, width * 0.1, height * 0.1, width - width * 0.2, height - height * 0.2, RectanglePlacement::stretchToFit);
 }
+
--- Source/CustomLookAndFeel.h
+++ Source/CustomLookAndFeel.h
@@ -2,18 +2,19 @@
 
 #include <JuceHeader.h>
 
-
 class CustomLookAndFeel : public LookAndFeel_V4 {
 public:
     CustomLookAndFeel();
     ~CustomLookAndFeel() {}
-    
+
     void drawRotarySlider(Graphics& g, int x, int y, int width, int height, float sliderPosProportional, float rotaryStartAngle, float rotaryEndAngle, Slider& slider) override;
+
+    void setNumTicks(int numTicks) { this->numTicks = numTicks; }
 
 private:
     Image knob;
     Image knobBase;
+    int numTicks = 10;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(CustomLookAndFeel)
 };
-
