8c36b5977dc9ce44cd081e5e369eb25f84e96c2a
Alessandro Sarchi <alessandro.sarchi02@gmail.com>
2024-07-27 01:08:00 +0200

    deleted scale factor costant

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -6,8 +6,6 @@
 #include "Meter.h"
 #include "TimedSlider.h"
 
-#define SCALE_FACTOR 1.4f
-
 typedef AudioProcessorValueTreeState::SliderAttachment SliderAttachment;
 
 class RalphComponent : public Component {
@@ -63,6 +61,6 @@ private:
     AudioProcessorValueTreeState& parameters;
     ApplicationProperties applicationProperties;
     
-    static constexpr int originalWidth  {static_cast<int>(800 * SCALE_FACTOR)};
-    static constexpr int originalHeight {static_cast<int>(600 * SCALE_FACTOR)};
+    static constexpr int originalWidth  {static_cast<int>(800)};
+    static constexpr int originalHeight {static_cast<int>(600)};
 };
